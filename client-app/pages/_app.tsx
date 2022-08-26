4afa1d678a1eefad6124c7d05237930438c0b0e0
Tor Andreas Johnsen <andru_joh@yahoo.no>
2022-08-26 19:37:22 +0200

    Cors and changes

--- client-app/pages/_app.tsx
+++ client-app/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const Layout = (props: { children: JSX.Element }) => {
+  return props.children;
+};
+
+function Index({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default Index;
